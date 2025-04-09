08aff836d66ca4d2201e1863ba500bd093e64a64
amirali-lll <amirali.dst.lll@gmail.com>
2025-04-09 23:45:56 +0330

    Add DocumentAdmin class to manage Document model in admin interface

--- document/admin.py
+++ document/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'created_at', 'updated_at')
+    search_fields = ('title', 'owner__username')
+    list_filter = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('owner')
+    
+    def owner(self, obj):
+        return obj.owner.username if obj.owner else 'Unknown'
+    
