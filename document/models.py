54a9e5dcca188bd66880ae5fae71f398b3507625
Hamed <www.hmdsdt@gmail.com>
2025-04-23 05:08:16 -0700

    added share code creation

--- document/models.py
+++ document/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Document(models.Model):
     title = models.CharField(max_length=255)
+    link = models.CharField(max_length=15,unique=True,blank=True)
     owner = models.ForeignKey('authentication.User', on_delete=models.SET_NULL, null=True, blank=True)
     content = models.BinaryField()
     created_at = models.DateTimeField(auto_now_add=True)
