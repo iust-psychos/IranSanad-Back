3edb6e94d0ec2f765a54aceb8162c28a4821426b
amirali-lll <amirali.dst.lll@gmail.com>
2025-04-24 18:00:16 +0330

    Add owner_name field to DocumentSerializer and update fields in Meta class

--- document/serializers.py
+++ document/serializers.py
@@ -1,22 +1,31 @@
+import logging
 from rest_framework import serializers
 from .models import *
 from .utility import link_generator
 
-class DocumentSerializer(serializers.ModelSerializer):    
+logger = logging.getLogger(__name__)
+
+class DocumentSerializer(serializers.ModelSerializer):   
+    # add owner_name filed as method 
+    owner_name = serializers.SerializerMethodField(method_name='get_owner_name')
+    
+    def get_owner_name(self, obj):
+        request_user = self.context.get('request').user
+        if request_user and request_user.id == obj.owner.id:
+            return 'Me'
+        return obj.owner.username if obj.owner else 'Unknown'
     class Meta:
         model = Document
-        fields = ['id', 'title', 'owner', 'created_at', 'updated_at', 'link']
-        read_only_fields = ['id', 'owner', 'created_at', 'updated_at', 'link']
+        fields = ['id', 'doc_uuid', 'title', 'owner', 'owner_name', 'created_at', 'updated_at', 'link']
+        read_only_fields = ['id', 'doc_uuid', 'owner', 'created_at', 'updated_at', 'link']
 
     def create(self, validated_data):
-        print('create called')
-        # Create the document first
         document = Document.objects.create(**validated_data)
-        print('after creation caled')
-        # Generate and set the link
         generated_link = link_generator(f"{document.title}{document.created_at.timestamp()}")
-        print(f'*********************{generated_link}*************************')
+
         document.link = generated_link
         document.save()
+        
+        logger.info(f"Document created with link: {generated_link}")
         
         return document
