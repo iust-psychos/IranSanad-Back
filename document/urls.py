7aa1b1061fd1838d37d0647e5df079aeace15ced
K-T-P <koroush.t81@gmail.com>
2025-05-18 23:31:25 +0330

    fix: document.id changed to document.uuid, author.id changed to author.uuid .

--- document/urls.py
+++ document/urls.py
@@ -5,7 +5,7 @@ router = DefaultRouter()
 router.register("", DocumentViewSet, "document")
 router.register("permission", DocumentPermissionViewSet, "permission")
 router.register(
-    r"document/(?P<doc_id>\d+)/comment",
+    r"document/(?P<doc_uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/comment",
     CommentViewSet,
     basename="comment",
 )
