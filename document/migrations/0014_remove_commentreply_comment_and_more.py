0c726644504c93a8217981ceb5c02e65137ab1f0
K-T-P <koroush.t81@gmail.com>
2025-05-21 13:07:43 +0330

    fix: Fix migrations conflict.

--- document/migrations/0014_remove_commentreply_comment_and_more.py
+++ document/migrations/0014_remove_commentreply_comment_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('document', '0012_alter_comment_range_end_alter_comment_range_start'),
+        ('document', '0013_documentupdate_author_documentupdate_authors_and_more'),
     ]
 
     operations = [
--- document/migrations/0015_comment_commentreply_and_more.py
+++ document/migrations/0015_comment_commentreply_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('document', '0013_remove_commentreply_comment_and_more'),
+        ('document', '0014_remove_commentreply_comment_and_more'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
