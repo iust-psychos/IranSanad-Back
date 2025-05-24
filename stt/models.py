de4dcffb1d2285c2130ace7546b432d1cb3362cd
amirali-lll <amirali.dst.lll@gmail.com>
2025-05-24 23:40:47 +0330

    feat: add initial migration and model for AudioTranscription

--- stt/models.py
+++ stt/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class AudioTranscription(models.Model):
+    user = models.ForeignKey(
+        'authentication.User', on_delete=models.SET_NULL, related_name='transcriptions', null=True, blank=True
+    )
+    audio_file = models.FileField(upload_to='audio_files/')
+    transcription = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transcription {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+    
